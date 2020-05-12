567f4347fb64de188bf7f396d6c0a23fe07a44db
timofey <timofey.gusev.opal@gmail.com>
2020-05-12 05:41:38 +0400

    Добавил подключение к БД

--- Form2.Designer.cs
+++ Form2.Designer.cs
@@ -153,6 +153,7 @@
 
         #endregion
 
+
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label clousButtonClick;
